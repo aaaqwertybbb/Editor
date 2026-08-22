327755c328eca84221d5e950fb56d333364483ad
Luthetus <luthetus@gmail.com>
2026-08-22 14:05:27 -0400

    well that's a massive reduction one of the leaks is gone, probably still more

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8770,8 +8770,6 @@ function EDITOR_mouseOver(e) {
     EDITOR_hoverTimeout = setTimeout(EDITOR_requestLspHover, 1000);
 }
 
-// you know when I called you a clown I was just projecting my own insecurities
-
 function EDITOR_mouseLeave() {
     // Clear timer if mouse leaves the token before 1000ms
     clearTimeout(EDITOR_hoverTimeout);

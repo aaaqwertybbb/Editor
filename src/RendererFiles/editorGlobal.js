064168dad686333ca246ec77ad9714ba77aefb00
Luthetus <luthetus@gmail.com>
2026-08-22 13:02:08 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -746,7 +746,7 @@ function EDITOR_onScroll_WRAPIT() {
  * In other words, attaching a function to an event handler which points back to its element can prevent browsers from garbage-collecting either.
  * (Thankfully, most newer browsers have "learned the trick" and no longer leak memory in this scenario, but there are a lot of older browsers floating around out there!)
  * 
- * 
+ * I'm still looking.
  */
 function EDITOR_render_do_Scroll(timestamp) {
 

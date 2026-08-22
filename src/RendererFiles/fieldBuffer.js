aeb23c045e2d137e9b2db8d5306c76ea86003ce4
Luthetus <luthetus@gmail.com>
2026-08-22 09:45:55 -0400

    get_EDITOR_gutterPaddingLeft

--- src/RendererFiles/fieldBuffer.js
+++ src/RendererFiles/fieldBuffer.js
@@ -162,11 +162,8 @@ const get_js_QUESTIONMARK_num = () => 63;
 const get_js_CARET_str = () => '^';
 const get_js_CARET_num = () => 94;
 
-const get_EDITOR_gutterPaddingLeft = () => EDITOR_byte_fields[34];
-EDITOR_byte_fields[34] = 3;
-
-const get_EDITOR_gutterPaddingRight = () => EDITOR_byte_fields[35];
-EDITOR_byte_fields[35] = 6;
+const get_EDITOR_gutterPaddingLeft = () => 3;
+const get_EDITOR_gutterPaddingRight = () => 6;
 
 const get_DIALOG_minTop = () => 8;
 const get_DIALOG_minLeft = () => 8;

da1a8cbfd3f2f07a27bec128e07311362aeb0aa6
Luthetus <luthetus@gmail.com>
2026-08-25 09:37:56 -0400

    Update fieldBuffer.js

--- src/RendererFiles/fieldBuffer.js
+++ src/RendererFiles/fieldBuffer.js
@@ -161,24 +161,19 @@ const CONST_DIALOG_minWidth = 100;
 ////////////////////////////
 ////////////////////////////
 
-// TODO: index 10 is available because 'ticket_didChangeTextDocumentNotificationPromise' was removed.
-// TODO: index 4 is available because 'EDITOR_isSourceOfLeftMouseButton' was removed.
 const EDITOR_int_fields = new Uint32Array(64);
 
-const get_EDITOR_lineHeight = () => EDITOR_int_fields[0];
 const set_EDITOR_lineHeight = (int) => EDITOR_int_fields[0] = int;
 const INDEXOF_EDITOR_lineHeight = 0;
 set_EDITOR_lineHeight(20);
 
 /** The first line of text that you should see shown in the UI given the current scrollTop */
-const get_EDITOR_virtualIndexLine = () => EDITOR_int_fields[1];
 const set_EDITOR_virtualIndexLine = (int) => EDITOR_int_fields[1] = int;
 const INDEXOF_EDITOR_virtualIndexLine = 1;
 
 /** The value of 'EDITOR_baseElement.scrollTop' at the most recent scroll event that occurred */
 const INDEXOF_lastReadNumber_scrollTop = 2;
 
-const get_EDITOR_ONSCROLLvirtualIndexLine = () => EDITOR_int_fields[3];
 const set_EDITOR_ONSCROLLvirtualIndexLine = (int) => EDITOR_int_fields[3] = int;
 const INDEXOF_EDITOR_ONSCROLLvirtualIndexLine = 3;
 //throw new Error('-1');
@@ -186,20 +181,16 @@ const INDEXOF_EDITOR_ONSCROLLvirtualIndexLine = 3;
 set_EDITOR_ONSCROLLvirtualIndexLine(500);
 
 /** Also is used from 'EDITOR_render_do_SetText()', and 'EDITOR_render_do_Resize()', not just 'EDITOR_render_do_Scroll()' */
-const get_EDITOR_scrollEndDeadline = () => EDITOR_int_fields[4];
-/** Also is used from 'EDITOR_render_do_SetText()', and 'EDITOR_render_do_Resize()', not just 'EDITOR_render_do_Scroll()' */
 const set_EDITOR_scrollEndDeadline = (int) => EDITOR_int_fields[4] = int;
 /** Also is used from 'EDITOR_render_do_SetText()', and 'EDITOR_render_do_Resize()', not just 'EDITOR_render_do_Scroll()' */
 const INDEXOF_EDITOR_scrollEndDeadline = 4;
 
-const get_EDITOR_ONSCROLLscrollTop = () => EDITOR_int_fields[5];
 const set_EDITOR_ONSCROLLscrollTop = (int) => EDITOR_int_fields[5] = int;
 const INDEXOF_EDITOR_ONSCROLLscrollTop = 5;
 //throw new Error('-1');
 // This set used to be -1 to indicate a non existent value, 500 "seems to work" but a proof of it being an equivalent solution has not thoroughly been thought out, only a sort of "yeah that probably works" kinda vibe.
 set_EDITOR_ONSCROLLscrollTop(500);
 
-const get_EDITOR_virtualCount = () => EDITOR_int_fields[6];
 const set_EDITOR_virtualCount = (int) => EDITOR_int_fields[6] = int;
 const INDEXOF_EDITOR_virtualCount = 6;
 
@@ -210,19 +201,19 @@ EDITOR_int_fields[INDEXOF_EDITOR_ONSCROLLvirtualCount] = 0;
 
 const INDEXOF_EDITOR_sum_diffNegative = 9;
 
-const get_EDITOR_findOverlay_isBeingShownDueToMultiCursorMatching_originMatchNumber = () => EDITOR_int_fields[10];
 const set_EDITOR_findOverlay_isBeingShownDueToMultiCursorMatching_originMatchNumber = (int) => EDITOR_int_fields[10] = int;
+const INDEXOF_EDITOR_findOverlay_isBeingShownDueToMultiCursorMatching_originMatchNumber = 10;
 
-const get_EDITOR_drawn_count_of_digits_longest_line_number = () => EDITOR_int_fields[11];
+const INDEXOF_EDITOR_drawn_count_of_digits_longest_line_number = 11;
 const set_EDITOR_drawn_count_of_digits_longest_line_number = (int) => EDITOR_int_fields[11] = int;
 
-const get_EDITOR_detail_smallPosition = () => EDITOR_int_fields[12];
+const INDEXOF_EDITOR_detail_smallPosition = 12;
 const set_EDITOR_detail_smallPosition = (int) => EDITOR_int_fields[12] = int;
 
-const get_EDITOR_detail_largePosition = () => EDITOR_int_fields[13];
+const INDEXOF_EDITOR_detail_largePosition = 13;
 const set_EDITOR_detail_largePosition = (int) => EDITOR_int_fields[13] = int;
 
-const get_EDITOR_detailRank3OriginLine = () => EDITOR_int_fields[14];
+const INDEXOF_EDITOR_detailRank3OriginLine = 14;
 const set_EDITOR_detailRank3OriginLine = (int) => EDITOR_int_fields[14] = int;
 
 /**
@@ -233,20 +224,20 @@ const set_EDITOR_detailRank3OriginLine = (int) => EDITOR_int_fields[14] = int;
  * 
  * Whereas the line height is a css variable (and thus could cause layout for the entire application whenever it changes).
  */
-const get_EDITOR_gutterWidthStyleValue = () => EDITOR_int_fields[15];
+const INDEXOF_EDITOR_gutterWidthStyleValue = 15;
 const set_EDITOR_gutterWidthStyleValue = (int) => EDITOR_int_fields[15] = int;
 set_EDITOR_gutterWidthStyleValue(32);
 
 /**
- * This is the sum of the 'get_EDITOR_gutterWidthStyleValue()' in addition to the left and right padding
+ * This is the sum of the 'INDEXOF_EDITOR_gutterWidthStyleValue()' in addition to paddig
  * consider 'gutterWidthTotal_withPxUnits'
  */
-const get_EDITOR_gutterWidthTotal = () => EDITOR_int_fields[16];
+const INDEXOF_EDITOR_gutterWidthTotal = 16;
 /** WARNING: This will not set 'gutterWidthTotal_withPxUnits' and thus is somewhat prone to a mistake at some point. */
 const set_EDITOR_gutterWidthTotal = (int) => EDITOR_int_fields[16] = int;
 set_EDITOR_gutterWidthTotal(32);
 
-const get_didChangeTextDocument_version = () => EDITOR_int_fields[17];
+const INDEXOF_didChangeTextDocument_version = 17;
 const set_didChangeTextDocument_version = (int) => EDITOR_int_fields[17] = int;
 
 /**
@@ -254,60 +245,58 @@ const set_didChangeTextDocument_version = (int) => EDITOR_int_fields[17] = int;
  * I'm experimenting with a few of the loops though such that at the start of every loop they set this variable equal to 'i'.
  * Then in any functions like getCharacter, I might be able to contextually find the character much faster.
  * */
-const get_EDITOR_indexCursor = () => EDITOR_int_fields[18];
+const INDEXOF_EDITOR_indexCursor = 18;
 const set_EDITOR_indexCursor = (int) => EDITOR_int_fields[18] = int;
 
-const get_EDITOR_offsetLine = () => EDITOR_int_fields[19];
+const INDEXOF_EDITOR_offsetLine = 19;
 const set_EDITOR_offsetLine = (int) => EDITOR_int_fields[19] = int;
 
-const get_EDITOR_offsetColumn_withRespectToThisIndexLine = () => EDITOR_int_fields[20];
+const INDEXOF_EDITOR_offsetColumn_withRespectToThisIndexLine = 20;
 const set_EDITOR_offsetColumn_withRespectToThisIndexLine = (int) => EDITOR_int_fields[20] = int;
 
-const get_EDITOR_offsetColumn = () => EDITOR_int_fields[21];
+const INDEXOF_EDITOR_offsetColumn = 21;
 const set_EDITOR_offsetColumn = (int) => EDITOR_int_fields[21] = int;
 
-const get_EDITOR_totalShift = () => EDITOR_int_fields[22];
+const INDEXOF_EDITOR_totalShift = 22;
 const set_EDITOR_totalShift = (int) => EDITOR_int_fields[22] = int;
 
-const get_EDITOR_offsetWithinSpan = () => EDITOR_int_fields[23];
+const INDEXOF_EDITOR_offsetWithinSpan = 23;
 const set_EDITOR_offsetWithinSpan = (int) => EDITOR_int_fields[23] = int;
 
-const get_EDITOR_longestLine_indexLine = () => EDITOR_int_fields[24];
 const set_EDITOR_longestLine_indexLine = (int) => EDITOR_int_fields[24] = int;
 const INDEXOF_EDITOR_longestLine_indexLine = 24;
 
-const get_EDITOR_longestLine_length = () => EDITOR_int_fields[25];
 const set_EDITOR_longestLine_length = (int) => EDITOR_int_fields[25] = int;
 const INDEXOF_EDITOR_longestLine_length = 25;
 
 /**
- * The get_EDITOR_contentWidth() is calculated via Math.ceil(someVar * otherVar) so this is faster to check whether content width will change rather than the multiplication and ceil.
+ * The INDEXOF_EDITOR_contentWidth() is calculated via Math.ceil(someVar * otherVar) so this is faster to check whether content width will change rather than the multiplication and ceil.
  */
-const get_EDITOR_longestLine_length_PreviousValueWhenLastDrewHorizontalScrollbar = () => EDITOR_int_fields[26];
+const INDEXOF_EDITOR_longestLine_length_PreviousValueWhenLastDrewHorizontalScrollbar = 26;
 const set_EDITOR_longestLine_length_PreviousValueWhenLastDrewHorizontalScrollbar = (int) => EDITOR_int_fields[26] = int;
 
-const get_EDITOR_contentWidth = () => EDITOR_int_fields[27];
+const INDEXOF_EDITOR_contentWidth = 27;
 const set_EDITOR_contentWidth = (int) => EDITOR_int_fields[27] = int;
 
-const get_EDITOR_indent_ORIGINAL_indentBy = () => EDITOR_int_fields[28];
+const INDEXOF_EDITOR_indent_ORIGINAL_indentBy = 28;
 const set_EDITOR_indent_ORIGINAL_indentBy = (int) => EDITOR_int_fields[28] = int;
 
-const get_EDITOR_indent_SMALL_lineAndColumnIndices_indexLine = () => EDITOR_int_fields[29];
+const INDEXOF_EDITOR_indent_SMALL_lineAndColumnIndices_indexLine = 29;
 const set_EDITOR_indent_SMALL_lineAndColumnIndices_indexLine = (int) => EDITOR_int_fields[29] = int;
 
-const get_EDITOR_indent_startingIndex = () => EDITOR_int_fields[30];
+const INDEXOF_EDITOR_indent_startingIndex = 30;
 const set_EDITOR_indent_startingIndex = (int) => EDITOR_int_fields[30] = int;
 
-const get_EDITOR_recentBoundingClientRect_left = () => EDITOR_int_fields[31];
+const INDEXOF_EDITOR_recentBoundingClientRect_left = 31;
 const set_EDITOR_recentBoundingClientRect_left = (int) => EDITOR_int_fields[31] = int;
 
-const get_EDITOR_recentBoundingClientRect_top = () => EDITOR_int_fields[32];
+const INDEXOF_EDITOR_recentBoundingClientRect_top = 32;
 const set_EDITOR_recentBoundingClientRect_top = (int) => EDITOR_int_fields[32] = int;
 
-const get_EDITOR_pooledTrackedSyntax_start = () => EDITOR_int_fields[33];
+const INDEXOF_EDITOR_pooledTrackedSyntax_start = 33;
 const set_EDITOR_pooledTrackedSyntax_start = (int) => EDITOR_int_fields[33] = int;
 
-const get_EDITOR_pooledTrackedSyntax_length = () => EDITOR_int_fields[34];
+const INDEXOF_EDITOR_pooledTrackedSyntax_length = 34;
 const set_EDITOR_pooledTrackedSyntax_length = (int) => EDITOR_int_fields[34] = int;
 
 

cfbae3913f6513ee138c2e4dab48533fbaaebba5
Luthetus <luthetus@gmail.com>
2026-08-26 19:20:39 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -9054,5 +9054,6 @@ The rocket emoji looks like '=?' but the '?' is actually surrounded by a diamond
 
 I gotta calm down cuz it is almost my bed time
 I feel so good right now
+but yeah I gotta calm down so I can go to bed on time
 
 */

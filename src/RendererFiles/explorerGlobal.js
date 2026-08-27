fe3d8225fe87c38fc7ff392b8945bc377e975b5f
Luthetus <luthetus@gmail.com>
2026-08-27 08:56:33 -0400

    does not build: EXPLORER_TreeViewDirector_event_dblclick

--- src/RendererFiles/explorerGlobal.js
+++ src/RendererFiles/explorerGlobal.js
@@ -1038,35 +1038,35 @@ function EXPLORER_TreeViewDirector_event_click(event_clientY, event_target) {
 }
 
 function EXPLORER_TreeViewDirector_event_dblclick(event_clientY, event_target) {
-    this.ensure_boundingClientRect();
+    EXPLORER_TreeViewDirector_ensure_boundingClientRect();
 
-    let rY = event_clientY - this.boundingClientRect.top + this.lastReadNumber_scrollTop;
-    let indexItem = Math.floor(rY / this.itemHeightNumber);
-    indexItem = this.state_cursor_validateIndex(indexItem);
+    let rY = event_clientY - EXPLORER_TreeViewDirector_boundingClientRect.top + EXPLORER_TreeViewDirector_lastReadNumber_scrollTop;
+    let indexItem = Math.floor(rY / EXPLORER_TreeViewDirector_itemHeightNumber);
+    indexItem = EXPLORER_TreeViewDirector_state_cursor_validateIndex(indexItem);
 
-    // TODO: This is an awkward explicit inlining of 'this.indexItemTo_beltIndexItem'...
+    // TODO: This is an awkward explicit inlining of 'EXPLORER_TreeViewDirector_indexItemTo_beltIndexItem'...
     // ...the initial declaration of 'let beltIndexLine' is assigned what I refer to as the "virtualIndex"
     // but 'beltIndexLine' is the output of the function, and a 'virtualIndex' variable is only needed temporarily
     // for the calculation. So by storing the 'virtualIndex' in 'beltIndexLine' at the start I skip a variable declaration.
-    let beltIndexItem = ((indexItem)) - this.virtualIndex_ofScrollTop;
-    if (beltIndexItem >= this.TREEVIEW_ArrayFrom_itemListElement_children_length || beltIndexItem < 0) beltIndexItem = -1;
-    else beltIndexItem = (beltIndexItem + this.beltIndexZero) % this.virtualCount;
+    let beltIndexItem = ((indexItem)) - EXPLORER_TreeViewDirector_virtualIndex_ofScrollTop;
+    if (beltIndexItem >= EXPLORER_TreeViewDirector_TREEVIEW_ArrayFrom_itemListElement_children_length || beltIndexItem < 0) beltIndexItem = -1;
+    else beltIndexItem = (beltIndexItem + EXPLORER_TreeViewDirector_beltIndexZero) % EXPLORER_TreeViewDirector_virtualCount;
 
     if (beltIndexItem < 0) return;
-    let divItem = this.TREEVIEW_ArrayFrom_itemListElement_children[beltIndexItem];
+    let divItem = EXPLORER_TreeViewDirector_TREEVIEW_ArrayFrom_itemListElement_children[beltIndexItem];
 
     // if not clicked "chevron"
     if (event_target !== divItem.children[0]) {
-        // TODO: This is an awkward explicit inlining of 'this.indexItemTo_beltIndexItem'...
+        // TODO: This is an awkward explicit inlining of 'EXPLORER_TreeViewDirector_indexItemTo_beltIndexItem'...
         // ...the initial declaration of 'let beltIndexLine' is assigned what I refer to as the "virtualIndex"
         // but 'beltIndexLine' is the output of the function, and a 'virtualIndex' variable is only needed temporarily
         // for the calculation. So by storing the 'virtualIndex' in 'beltIndexLine' at the start I skip a variable declaration.
-        let beltIndexItem = ((this.cursorIndex)) - this.virtualIndex_ofScrollTop;
-        if (beltIndexItem >= this.TREEVIEW_ArrayFrom_itemListElement_children_length || beltIndexItem < 0) beltIndexItem = -1;
-        else beltIndexItem = (beltIndexItem + this.beltIndexZero) % this.virtualCount;
+        let beltIndexItem = ((EXPLORER_TreeViewDirector_cursorIndex)) - EXPLORER_TreeViewDirector_virtualIndex_ofScrollTop;
+        if (beltIndexItem >= EXPLORER_TreeViewDirector_TREEVIEW_ArrayFrom_itemListElement_children_length || beltIndexItem < 0) beltIndexItem = -1;
+        else beltIndexItem = (beltIndexItem + EXPLORER_TreeViewDirector_beltIndexZero) % EXPLORER_TreeViewDirector_virtualCount;
 
         if (beltIndexItem < 0) return;
-        return this.tvd_ondblclick_async(this.TREEVIEW_ArrayFrom_itemListElement_children[beltIndexItem], this.cursorIndex);
+        return EXPLORER_TreeViewDirector_tvd_ondblclick_async(EXPLORER_TreeViewDirector_TREEVIEW_ArrayFrom_itemListElement_children[beltIndexItem], EXPLORER_TreeViewDirector_cursorIndex);
     }
 }
 

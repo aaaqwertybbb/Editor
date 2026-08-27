beda90683a3718dee7ace9298b6cb6a68cea4452
Luthetus <luthetus@gmail.com>
2026-08-27 08:36:58 -0400

    does not build: EXPLORER_TreeViewDirector_tvd_ondblclick_async

--- src/RendererFiles/explorerGlobal.js
+++ src/RendererFiles/explorerGlobal.js
@@ -481,7 +481,7 @@ async function EXPLORER_TreeViewDirector_tvd_onkeydown_async(divItem, indexItem,
 }
 
 async function EXPLORER_TreeViewDirector_tvd_ondblclick_async(divItem, indexItem) {
-    this.nodeList.getElementAt(indexItem);
+    EXPLORER_TreeViewDirector_nodeList.getElementAt(indexItem);
     let key = gINT_FIELDS[fTreeView_pooledNode_key];
     let depth = gINT_FIELDS[fTreeView_pooledNode_depth];
     let nodeKind = gBYTE_FIELDS[byteTreeView_pooledNode_nodeKind];

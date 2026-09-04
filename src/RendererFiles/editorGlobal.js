aafdc0eee73dbabc02490dad9752913e6fc6a0b2
Luthetus <luthetus@gmail.com>
2026-09-04 13:44:34 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8337,4 +8337,8 @@ Why are you so jealous of me
 Or I'll get recommended videos of pirate software especially ones of his massive array that he puts tons of stuff in.
 You literally aren't reading what I'm saying then cause I'm saying over and over 99.9% of code should not look like that.
 
+"tonight I'm getting over you"
+"we are never getting back together"
+I am just in constant fight or flight because I see these things
+
 */

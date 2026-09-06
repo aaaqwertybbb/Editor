b5b9a2a5e6b83fc6a213c6d75299fbd8e8e7ec22
Luthetus <luthetus@gmail.com>
2026-09-06 09:26:53 -0400

    rename to 'EXPLORER_event_dblclick'

--- src/RendererFiles/explorerGlobal.js
+++ src/RendererFiles/explorerGlobal.js
@@ -826,7 +826,7 @@ function EXPLORER_TreeViewDirector_draw_addEvents() {
     EXPLORER_rootElement.addEventListener('click', EXPLORER_event_click); // this.event_click(event.clientY, event.target);
     EXPLORER_rootElement.addEventListener('keydown', EXPLORER_event_keydown); // this.event_keydown(event);
     EXPLORER_rootElement.addEventListener('scroll', EXPLORER_event_scroll, { passive: true }); // this.event_scroll();
-    EXPLORER_rootElement.addEventListener('dblclick', EXPLORER_TreeViewDirector_event_dblclick); // this.event_dblclick(event.clientY, event.target);
+    EXPLORER_rootElement.addEventListener('dblclick', EXPLORER_event_dblclick); // this.event_dblclick(event.clientY, event.target);
     EXPLORER_rootElement.addEventListener('contextmenu', EXPLORER_TreeViewDirector_event_contextmenu); // this.event_contextmenu(event.button, event.clientX, event.clientY);
     window.addEventListener('resize', EXPLORER_TreeViewDirector_event_windowResize); // this.event_windowResize();
 }
@@ -835,7 +835,7 @@ function EXPLORER_TreeViewDirector_draw_removeEvents() {
     EXPLORER_rootElement.removeEventListener('click', EXPLORER_event_click);
     EXPLORER_rootElement.removeEventListener('keydown', EXPLORER_event_keydown);
     EXPLORER_rootElement.removeEventListener('scroll', EXPLORER_event_scroll, { passive: true });
-    EXPLORER_rootElement.addEventListener('dblclick', EXPLORER_TreeViewDirector_event_dblclick);
+    EXPLORER_rootElement.addEventListener('dblclick', EXPLORER_event_dblclick);
     EXPLORER_rootElement.addEventListener('contextmenu', EXPLORER_TreeViewDirector_event_contextmenu);
     window.removeEventListener('resize', EXPLORER_TreeViewDirector_event_windowResize);
 }
@@ -1003,7 +1003,7 @@ function EXPLORER_event_click(event) {
     }
 }
 
-function EXPLORER_TreeViewDirector_event_dblclick(event) {
+function EXPLORER_event_dblclick(event) {
 
     // this.event_dblclick(event.clientY, event.target);
     let event_clientY = event.clientY;

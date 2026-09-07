d0e5f1aba0a3478600a9edd38ea43992bb8202b8
Luthetus <luthetus@gmail.com>
2026-09-07 15:00:11 -0400

    does not run: lineEndPositionList changing

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -394,7 +394,126 @@ const EDI_lineEndPositionList_PENDING = new UInt32List(128);
  */
 const EDI_lineEndPositionList = new UInt32List(128);
 
+/*
+this.capacity = initialCapacity;
+EDI_lineEndPositionList.data
+EDI_lineEndPositionList.clear()
+EDI_lineEndPositionList.count
+EDI_lineEndPositionList.insert()
+EDI_lineEndPositionList.removeAt()
+*/
 
+let EDI_lineEndPositionList_capacity = 128;
+let EDI_lineEndPositionList_data = new Uint32Array(EDI_lineEndPositionList_capacity);
+let EDI_lineEndPositionList_count = 0;
+/**
+ * Does not clear the information, only sets 'this.count' to '0'.
+ */
+function EDI_lineEndPositionList_clear() {
+    this.count = 0;
+}
+/**
+ * TODO: ensure all the parameters are encoded, especially because I'm noticing myself forgetting.
+ */
+function EDI_lineEndPositionList_insert(index, int32Value) {
+    this.ensureCapacityForInsertion(index, 1);
+
+    if (index !== this.count) {
+        this.copyTo(this.data, index, this.data, index + 1, this.count - index);
+    }
+
+    this.data[index] = int32Value;
+
+    this.count++;
+}
+/**
+ * Does not clear trailing information.
+ * 
+ * count === 0 immediately returns
+ */
+function EDI_lineEndPositionList_removeAt(index, count) {
+    if (index > this.count) { throw new Error('removeAt(...): index > this.count'); }
+    if (index + count > this.count) { throw new Error('removeAt(...): index + count > this.count'); }
+    if (count === 0) { return; }
+
+    if (index + count === this.count) {
+        let shiftableCount = this.count - (index + count);
+        if (shiftableCount > 0) {
+            this.copyTo(
+                this.data,
+                index + count,
+                this.data,
+                index,
+                shiftableCount);
+        }
+    }
+    else {
+        this.copyTo(
+            this.data,
+            index + count,
+            this.data,
+            index,
+            this.count - (index + count));
+    }
+
+    this.count -= count;
+}
+/**
+ * - If the size asked for cannot be allocated, an exception will be thrown. (presumably the wording "thrown by the runtime" is involved.)
+ * - JavaScript numbers do not wrap around to negative values when the value is very large.
+ *       They instead approach infinity and lose precision.
+ *       - There still is a check for whether the new, expected to be larger, capacity is smaller for whatever reason.
+ *         Since this ought to be a negligible check for this method to perform.
+ *         And failure to catch that case if it happens is an infinite loop.
+ */
+function EDI_lineEndPositionList_ensureCapacityForInsertion(index, count) {
+    let capacityPrevious = this.capacity;
+    while (true) {
+        if (this.count + count > this.capacity) {
+            this.doubleCapacity();
+        }
+        else if (index >= this.capacity) {
+            this.doubleCapacity();
+        }
+        else {
+            break;
+        }
+
+        if (this.capacity === capacityPrevious) {
+            break;
+        }
+        if (this.capacity < capacityPrevious) {
+            throw new Error('ensureCapacityForInsertion(...): this.capacity < capacityPrevious');
+        }
+
+        capacityPrevious = this.capacity;
+    }
+}
+function EDI_lineEndPositionList_doubleCapacity() {
+    let capacityNew = this.capacity * 2;
+    let bytesNew = new Uint32Array(capacityNew);
+    this.copyTo(this.data, 0, bytesNew, 0, this.count);
+    this.data = bytesNew;
+    this.capacity = capacityNew;
+}
+/**
+ * inclusive/exclusive
+ */
+function EDI_lineEndPositionList_copyTo(bytesSource, sourceStart, bytesDestination, destinationStart, length) {
+    if (bytesSource === bytesDestination) {
+        if (bytesSource !== this.data) {
+            throw new Error('bytesSource === bytesDestination ; but bytesSource !== this');
+        }
+
+        this.data.copyWithin(destinationStart, sourceStart, sourceStart + length);
+    }
+    else {
+        // TODO: use 'set' method here and other such locations
+        for (var i = 0; i < length; i++) {
+            bytesDestination[destinationStart + i] = bytesSource[sourceStart + i];
+        }
+    }
+}
 
 
 

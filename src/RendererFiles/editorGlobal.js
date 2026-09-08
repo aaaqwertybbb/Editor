96bc6810d30473a34cee731970598e9e02c90723
Luthetus <luthetus@gmail.com>
2026-09-08 09:23:18 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8933,4 +8933,5 @@ const requiredCapacity = Math.max(EDI_textByteList_count + count, index + count)
 <
 < If you'd like, we can also look at optimizing how you shift the existing bytes over to make room for the insertion.
 
+
 */

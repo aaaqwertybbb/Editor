e4788ce0d4d52d83253b00d76f1d30f2726f9e56
Luthetus <luthetus@gmail.com>
2026-09-08 08:20:15 -0400

    8:20 AM

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8932,4 +8932,6 @@ const requiredCapacity = Math.max(EDI_textByteList_count + count, index + count)
 <
 < If you'd like, we can also look at optimizing how you shift the existing bytes over to make room for the insertion.
 
+8:20 AM
+
 */

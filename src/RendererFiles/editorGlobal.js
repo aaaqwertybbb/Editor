e074c746006d67dba136310b68ecce3e1d4caea6
Luthetus <luthetus@gmail.com>
2026-09-12 13:25:35 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8319,6 +8319,7 @@ I never seen the video
 
 but its like people think everythings just a bit
 
-
+I've heard "He Can't Keep Getting Away With This!" as in the phrase as a reference and it is used jokingly mockingly
+They're mocking me
 
 */

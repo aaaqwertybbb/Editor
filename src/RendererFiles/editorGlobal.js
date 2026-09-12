da3442e7261109a74ca5a884e9e20be052da0274
Luthetus <luthetus@gmail.com>
2026-09-12 10:50:34 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8266,6 +8266,8 @@ Somehow I need to determine how large the tabs are that appear to the left of my
 Everytime I think of the words "WAKE UP" I just think of "WAKE UP ILYA" and I start laughin
 
 "I'm incompetent at writing code"... "DRUGS, just solve all your problems with stimulants. It's just caffeine I mean that's practically a daily multivitamin in our society"
+I'm half a sleep
+
 
 
 */
